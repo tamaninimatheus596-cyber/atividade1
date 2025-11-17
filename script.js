5387235e6e666e36545a0d1ae7c90bac456cade1
tamaninimatheus596-cyber <matheus@gmail.com>
2025-11-17 11:40:22 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const botao = document.getElementById("btEnviar");
+
+botao.addEventListener("click", function() {
+    alert("Formulário enviado!");
+    
+});
+
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    alert(`Formulário enviado!`);
+
+
