1e12adb7eebeaad8b78a40bdbfa08a340503a2be
tamaninimatheus596-cyber <matheus@gmail.com>
2025-11-18 09:54:00 -0300

    terceiro commit, agora com botoes de sexo e opção de idade

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-
 const botao = document.getElementById("btEnviar");
 
-botao.addEventListener("click", function() {
-    alert("Formulário enviado!");
-    
-
-
+botao.addEventListener("click", function (event) {
+    event.preventDefault(); 
 
     const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
-    alert(`Formulário enviado!`);
 
+    alert("Formulário enviado!");
+});
 
+const sliderIdade = document.getElementById("idade");
+const idadeValor = document.getElementById("idadeValor");
+
+sliderIdade.addEventListener("input", () => {
+    idadeValor.textContent = sliderIdade.value;
 });
